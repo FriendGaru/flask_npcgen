21b95c3e9535b3a25116feef2c77e5bc1bda9efe
FriendGaru <friendgaru@gmail.com>
2018-10-02 02:06:12 +0900

    ech

--- flask_npcgen.py
+++ flask_npcgen.py
@@ -4,18 +4,22 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+wsgi_app = flask_app.wsgi_app
 
 from routes import *
 
+# if __name__ == '__main__':
+#     import os
+#     HOST = os.environ.get('SERVER_HOST', 'localhost')
+#     try:
+#         PORT = int(os.environ.get('SERVER_PORT', '5555'))
+#     except ValueError:
+#         PORT = 5555
+#     flask_app.run(HOST, PORT)
+
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+
+    flask_app.run()
