ab5ed54d768a03bbbb59da370e279bc9576bd1f1
FriendGaru <friendgaru@gmail.com>
2018-10-04 15:58:03 +0900

    various tweaks
    switched to GET because that will make it easier to redirect/reacquire npcs, added validation, both for new character parameters and emotionally, various fixes

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 import random
 import string
 from flask_npcgen import app
@@ -16,23 +16,46 @@ def hello():
     return 'hello'
 
 
-@app.route('/npc', methods=['GET', 'POST'])
+@app.route('/npc', methods=['GET'])
 def npc():
     global npc_generator
     if not npc_generator:
         npc_generator = genny.NPCGenerator()
 
-    if request.method == 'POST':
-        seed = request.form['seed']
-        race_choice = request.form['race']
-        class_choice = request.form['class']
-        hd = int(request.form['hd'])
-    else:
+    if request.method == 'GET':
         # for method == 'GET':
-        seed = random_string(10)
-        race_choice = npc_generator.get_random_option('race')
-        class_choice = npc_generator.get_random_option('class')
-        hd = random.randint(1, 20)
+        valid = True
+
+        if request.args.get('seed'):
+            seed = request.args.get('seed')
+        else:
+            valid = False
+            seed = random_string(10)
+
+        if request.args.get('race_choice') \
+                and npc_generator.validate_params(race_choice=request.args.get('race_choice')):
+            race_choice = request.args.get('race_choice')
+        else:
+            valid = False
+            race_choice = npc_generator.get_random_option('race')
+
+        if request.args.get('class_choice') \
+                and npc_generator.validate_params(class_choice=request.args.get('class_choice')):
+            class_choice = request.args.get('class_choice')
+        else:
+            valid = False
+            class_choice = npc_generator.get_random_option('class')
+
+        if request.args.get('hd') \
+                and npc_generator.validate_params(hd=request.args.get('hd')):
+            hd = int(request.args.get('hd'))
+        else:
+            valid = False
+            hd = random.randint(1, 20)
+
+        if not valid:
+            return redirect(url_for('npc', seed=seed, hd=str(hd),
+                                    race_choice=race_choice, class_choice=class_choice, ))
 
     new_npc = npc_generator.new_character(
         race_template_name=race_choice,
@@ -40,6 +63,7 @@ def npc():
         seed=seed,
         hit_dice_num=hd,
     )
+
     stat_block = new_npc.build_stat_block()
     plaintext = stat_block.plain_text()
     stat_block_dict = stat_block.get_dict()
@@ -52,6 +76,13 @@ def npc():
     race_options = npc_generator.get_options('race')
     class_options = npc_generator.get_options('class')
     hd_range = list(range(1, 21))
-    new_seed = random_string(10)
-    return render_template('npc.html', seed=seed, new_seed=new_seed, stat_block=stat_block_dict, race_options=race_options,
-                           class_options=class_options, hd_range=hd_range, plaintext=plaintext)
+    return render_template('npc.html',
+                           seed=seed,
+                           stat_block=stat_block_dict,
+                           race_options=race_options,
+                           race_choice=race_choice,
+                           class_options=class_options,
+                           class_choice=class_choice,
+                           hd_range=hd_range,
+                           hd=str(hd),
+                           plaintext=plaintext)
